fdf0468d3fef0df883e0948d1dea82ec631d3448
Reina <panyw513@gmail.com>
2024-06-17 21:20:35 +0800

    feat(shopping-cart-page): 新增購物車頁面實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ProductPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
