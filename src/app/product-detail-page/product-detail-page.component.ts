102870d73959503d1299cde74286f24cef5cfa8c
Reina <panyw513@gmail.com>
2024-06-18 15:32:35 +0800

    feat(pagination): 新增分頁元件

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -2,11 +2,12 @@ import { CurrencyPipe } from '@angular/common';
 import { Component, inject, Input, numberAttribute } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../model/product';
+import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 
 @Component({
   selector: 'app-product-detail-page',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, ProductCardListComponent],
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.css',
 })
